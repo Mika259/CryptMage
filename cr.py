4e0b48d900e178205eee5cc8d781095be32507b2
M1K259 <87774553+Mika259@users.noreply.github.com>
2024-04-17 09:42:42 +0800

    Create cr.py

---
+++ cr.py
@@ -0,0 +1,36 @@
+
+import base64 as EnY64
+#base coun sleep
+def b1():
+  return 2
+#return ma'n var
+def m6():
+  return 1
+bt = b1()
+rd = m6()
+#ins vall ma'n
+def coun():
+  return bt - rd
+from time import sleep as SCout
+#Image Cout var mean hide
+class vs:
+  image='data'
+vs.image=input("")
+print(f"File : {vs.image}")
+#str ayat ma'n class
+#make a show sent
+tryt=10
+for test in range(tryt):
+  pass
+try:
+ print("[Copy is True]")
+ SCout(coun())
+ with open(vs.image, "rb") as FileMIg:
+     IDmage=FileMIg.read()
+#read file far method
+     EnC64iMGw=EnY64.b64encode(IDmage).decode('utf-8')
+ print(EnC64iMGw)
+except Exception as eRno:
+ exit(eRno)
+#Play defF StFring
+# Mika259 :v
